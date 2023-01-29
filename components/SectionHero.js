5c664ace5255df3fdcdcefc24e8051e9421f980f
Justin Mountain <moun0048@algonquinlive.com>
2023-01-29 10:09:26 -0500

    Fixed apostrophe and added alt text to socials

--- components/SectionHero.js
+++ components/SectionHero.js
@@ -9,7 +9,7 @@ function SectionHero() {
         <title>Justin Mountain</title>
       </Head>
       <div className='text-center p-16 w-96 m-auto'>
-        <h2 className='text-3xl'>Hi, I'm Justin</h2>
+        <h2 className='text-3xl'>Hi, I&apos;m Justin</h2>
         <h3 className='text-xl p-2'>Junior Developer</h3>
         <p className='pt-4'>
           Currently pursuing interests in full stack web development and DevOps.
