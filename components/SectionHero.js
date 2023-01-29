c1ac34d12ba5fffbd121fd5e9b046555e5bce4d3
Justin Mountain <moun0048@algonquinlive.com>
2023-01-29 00:35:55 -0500

    head fixes

--- components/SectionHero.js
+++ components/SectionHero.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Head from 'next/head'
 import SocialLinks from '@/components/SocialLinks'
 
 function SectionHero() {
   return (
     <div className='text-center p-16 w-96 m-auto'>
+      <Head>
+        <title>Justin Mountain</title>
+      </Head>
       <h2 className='text-3xl'>Hi, Im Justin</h2>
       <h3 className='text-xl p-2'>Junior Developer</h3>
       <p className='pt-4'>
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/x-icon" href="/favicon.svg" />
+        <link rel="icon" type="image/x-icon" href="/icons/favicon.png?" />
       </Head>
       <body>
         <Main />
