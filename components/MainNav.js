e54ff7f5df38c093991b7bc75f2ac4a6b55f8a3c
Justin Mountain <moun0048@algonquinlive.com>
2023-02-13 20:06:44 -0500

    Fixed key:index error in MainNav

--- components/MainNav.js
+++ components/MainNav.js
@@ -5,12 +5,12 @@ export default function MainNav() {
 
   const navLinks = [
     {name:"Blog", href:"/"},
-    {name:"Contact", href:"/"},
+    {name:"Contact", href:"mailto:mountainj@gmail.com"},
   ]
 
   const navHtml = navLinks.map((link, index) => (
-    <li className="invert-link-color my-auto text-right pr-8 md:pr-0 py-8 md:py-0 md:mr-16 md:last:mr-0">
-      <Link key={index} href={link.href}>
+    <li key={index} className="invert-link-color my-auto text-right pr-8 md:pr-0 py-8 md:py-0 md:mr-16 md:last:mr-0">
+      <Link href={link.href}>
         {link.name}
       </Link>
     </li>
